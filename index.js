1388e450d5d239ea0d61696c59582d56fcdf9899
Matthew M. Provost <provostmat@gmail.com>
2023-04-26 18:29:13 -0400

    added employee class

---
+++ index.js
@@ -0,0 +1,6 @@
+const generateHTML = require('./src/generateHTML');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Manager');
+const Intern = require('./lib/Manager');
+const inquirer = require('inquirer');
+const fs = require('fs');
